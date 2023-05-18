cfda5edc62e3bf86b5862dddb73614c7b3c16ad2
Lincoln S. Rocha <lincoln@dc.ufc.br>
2023-05-18 09:01:36 -0300

    Adding unit test for calculator app.

---
+++ src/test/java/br/ufc/tpii/mycalc/CalcAppTest.java
@@ -0,0 +1,29 @@
+package br.ufc.tpii.mycalc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalcAppTest {
+
+	@Test
+	void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMinus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPlus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDiv() {
+		fail("Not yet implemented");
+	}
+
+}
