d38cc98b1f1fe0cb8e9f43edc5445c6daa23ecd7
Lincoln S. Rocha <lincoln@dc.ufc.br>
2023-05-18 08:52:40 -0300

    Creating the calculator app.

---
+++ src/main/java/br/ufc/tpii/mycalc/CalcApp.java
@@ -0,0 +1,20 @@
+package br.ufc.tpii.mycalc;
+
+public class CalcApp {
+
+	public double add(double a, double b) {
+		return a + b;
+	}
+
+	public double minus(double a, double b) {
+		return a - b;
+	}
+
+	public double plus(double a, double b) {
+		return a * b;
+	}
+
+	public double div(double a, double b) {
+		return a / b;
+	}
+}
